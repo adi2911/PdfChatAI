bba3726400ab91606145548a4a4a57f076ee2d3c
adjohari <adjohari25@gmail.com>
2024-06-20 22:00:31 +0530

    add conversational memory to stored in sqlite db

--- app/chat/vector_stores/pinecone.py
+++ app/chat/vector_stores/pinecone.py
@@ -9,3 +9,10 @@ pinecone.Pinecone(
 )
 
 vector_store = Pinecone.from_existing_index(os.getenv("PINECONE_INDEX_NAME"),embedding=embeddings)
+
+
+def build_retriever(chat_args):
+    search_kwargs = {"filter": {"pdf_id": chat_args.pdf_id}}
+    return vector_store.as_retriever(
+        search_kwargs=search_kwargs
+    )
